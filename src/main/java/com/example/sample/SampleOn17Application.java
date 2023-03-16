60dae96c78556e7c1995a008750ab2be1332fa31
pramod.zarekar <pramod.zarekar@gurucul.com>
2023-03-16 21:18:17 +0530

    Added sample project of springboot

---
+++ src/main/java/com/example/sample/SampleOn17Application.java
@@ -0,0 +1,13 @@
+package com.example.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleOn17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleOn17Application.class, args);
+	}
+
+}
